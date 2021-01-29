0015ecbc6a8690a6c522a0903fa2b75bfc672bb2
Tan Hernandez <tan.hernandez05@gmail.com>
2021-01-30 01:27:01 +0800

    Registered globalMixin

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,7 +33,9 @@ export default {
   },
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [
+    {src: '~/plugins/globalMixin', ssr: false}
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
---
+++ plugins/globalMixin.js
@@ -0,0 +1,8 @@
+import { getBem } from 'assets/js/helpers/components';
+import Vue from 'vue';
+
+Vue.mixin({
+  methods: {
+    getBem
+  }
+});

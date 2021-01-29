a9771d0c30901a234b48158bcd5aaa02e7e848f6
Tan Hernandez <tan.hernandez05@gmail.com>
2021-01-30 01:33:04 +0800

    Added and registered vuex persist.

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,7 +34,8 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    {src: '~/plugins/globalMixin', ssr: false}
+    {src: '~/plugins/globalMixin', ssr: false},
+    {src: '~/plugins/vuexPersist', ssr: false}
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components

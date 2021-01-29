1c88fcd233d9a6200e5ac95a3f42e7401858d3a1
Tan Hernandez <tan.hernandez05@gmail.com>
2021-01-30 03:29:19 +0800

    Updated header contents.

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,23 @@
 <template>
   <div :class="getBem(blockClass)">
-    <search-box v-model="search"/>
+    <div :class="getBem(blockClass, 'wrapper')">
+      <pokeball-float/>
+
+      <div :class="getBem(blockClass, 'header')">
+        <h1>Sophisticated Pokedex</h1>
+        <p>
+          Type the name of the pokemon that <br>you're looking for:
+        </p>
+        <search-box v-model="search" placeholder="Search for Name or ID"/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import SearchBox from '~/components/SearchBox';
 import PokemonCard from '~/components/PokemonCard';
+import PokeballFloat from '~/components/PokeballFloat';
 
 export default {
 
@@ -17,7 +28,8 @@ export default {
   */
   components: {
     SearchBox,
-    PokemonCard
+    PokemonCard,
+    PokeballFloat
   },
 
   data () {
@@ -30,5 +42,26 @@ export default {
 </script>
 
 <style lang="scss">
+.home-page {
+  position: relative;
 
+  &__wrapper {
+    max-width: 1200px;
+    width: 80%;
+    margin: 0 auto;
+  }
+
+  &__header {
+    position: relative;
+    padding-top: 59px;
+
+    h1 {
+      margin-top: 0;
+    }
+
+    p {
+      line-height: 1.5em;
+    }
+  }
+}
 </style>

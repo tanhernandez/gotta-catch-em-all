95027ac975e701355c4ce61447cd71d910fe93b8
Tan Hernandez <tan.hernandez05@gmail.com>
2021-01-30 01:40:33 +0800

    Added and registered @nuxtjs/svg.

--- nuxt.config.js
+++ nuxt.config.js
@@ -42,7 +42,9 @@ export default {
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [],
+  buildModules: [
+    "@nuxtjs/svg"
+  ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [

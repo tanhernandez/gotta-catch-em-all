d19e5758f378f685520d30c0a8efcae845ca30ab
Tan Hernandez <tan.hernandez05@gmail.com>
2021-01-30 03:27:05 +0800

    Added PokeballFloat

---
+++ components/PokeballFloat.vue
@@ -0,0 +1,77 @@
+<template>
+  <div :class="getBem(blockClass)" :style="imgStyle">
+    <img
+      :src="`/images/${imgSrc}`"
+      :alt="imgSrc"
+    >
+  </div>
+</template>
+
+<script>
+export default {
+
+  /*
+  |--------------------------------------------------------------------------
+  | Component > props
+  |--------------------------------------------------------------------------
+  */
+  props: {
+    color: {
+      type: String,
+      default: 'black'
+    }
+  },
+
+  /*
+  |--------------------------------------------------------------------------
+  | Component > data
+  |--------------------------------------------------------------------------
+  */
+  data () {
+    return {
+      blockClass: 'pokeball-float'
+    }
+  },
+
+  /*
+  |--------------------------------------------------------------------------
+  | Component > computed
+  |--------------------------------------------------------------------------
+  */
+  computed: {
+
+    /**
+     * @returns {string}
+     */
+    imgSrc () {
+      return this.color === 'black'
+             ? 'pokeball-silhoutte-black.png'
+             : 'pokeball-silhoutte-white.png';
+    },
+
+    /**
+     * @returns {object}
+     */
+    imgStyle () {
+      return this.color === 'black'
+             ? {opacity: .07}
+             : {opacity: .2};
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.pokeball-float {
+  position: absolute;
+  top: -70px;
+  right: -70px;
+  width: 300px;
+  height: 300px;
+
+  img {
+    width: 100%;
+    height: auto;
+  }
+}
+</style>

a5b3678e37d10a76c8175bb430e587e475003163
Tan Hernandez <tan.hernandez05@gmail.com>
2021-01-30 05:00:06 +0800

    Updated content for home page.

--- pages/index.vue
+++ pages/index.vue
@@ -4,12 +4,46 @@
       <pokeball-float/>
 
       <div :class="getBem(blockClass, 'header')">
-        <h1>Sophisticated Pokedex</h1>
+        <h1>Pokédex</h1>
+        <span :class="getBem(blockClass, 'header-subtitle')">
+          Modern Nintendo theme.
+        </span>
         <p>
-          Type the name of the pokemon that <br>you're looking for:
+          Type the name of the Pokémon that <br>you're looking for:
         </p>
-        <search-box v-model="search" placeholder="Search for Name or ID"/>
+        <search-box
+          v-model="search"
+          placeholder="Search for Name or ID"
+          @change="setFormattedList"
+          @reset="setFormattedList"
+        />
       </div>
+
+      <div :class="getBem(blockClass, 'body')">
+        <template v-if="formattedList.length > 0">
+          <div class="pokemon-card-container">
+            <pokemon-card
+              v-for="pokemon in formattedList"
+              :key="pokemon.id"
+              :pokemon="pokemon"
+            />
+          </div>
+        </template>
+        <template v-else>
+          <p>
+            There are no Pokémons found.
+          </p>
+        </template>
+      </div>
+
+      <a
+        v-if="search === ''"
+        href="#"
+        :class="getBem(blockClass, 'load-more-button')"
+        @click.prevent="handleClickLoadMore"
+      >
+        Load more Pokémon!
+      </a>
     </div>
   </div>
 </template>
@@ -18,6 +52,8 @@
 import SearchBox from '~/components/SearchBox';
 import PokemonCard from '~/components/PokemonCard';
 import PokeballFloat from '~/components/PokeballFloat';
+import filter from 'lodash/filter';
+import includes from 'lodash/includes';
 
 export default {
 
@@ -32,11 +68,65 @@ export default {
     PokeballFloat
   },
 
+  /*
+  |--------------------------------------------------------------------------
+  | Component > data
+  |--------------------------------------------------------------------------
+  */
   data () {
     return {
       blockClass: 'home-page',
-      search: ''
+      search: '',
+      formattedList: []
     }
+  },
+
+  /*
+  |--------------------------------------------------------------------------
+  | Component > methods
+  |--------------------------------------------------------------------------
+  */
+  methods: {
+
+    /**
+     * @return {array}
+     */
+    setFormattedList () {
+      if (this.search !== '') {
+        this.formattedList = filter(this.$store.getters['pokemon/fullList'], (x) => {
+          // noinspection EqualityComparisonWithCoercionJS
+          return includes(x.name, this.search) || x.id == this.search;
+        });
+      } else {
+        this.formattedList = this.$store.getters['pokemon/paginatedList'];
+      }
+    },
+
+    /**
+     * @return {void}
+     */
+    handleClickLoadMore () {
+      this.search = '';
+      this.$store.commit(
+        'pokemon/setListPage',
+        this.$store.getters['pokemon/listCurrentPage'] + 1
+      );
+      this.setFormattedList();
+      setTimeout(() => {
+        window.scrollTo({top: document.body.scrollHeight, behavior: 'smooth'});
+      }, 500);
+    }
+  },
+
+  /*
+  |--------------------------------------------------------------------------
+  | Component > mounted
+  |--------------------------------------------------------------------------
+  */
+  mounted () {
+    this.$store.dispatch('pokemon/listAll').then(() => {
+      this.setFormattedList();
+    });
   }
 }
 </script>
@@ -57,11 +147,52 @@ export default {
 
     h1 {
       margin-top: 0;
+      margin-bottom: 0;
+    }
+
+    &-subtitle {
+      font-size: 12px;
     }
 
     p {
       line-height: 1.5em;
     }
   }
+
+  &__body {
+    padding-top: 50px;
+  }
+
+  &__load-more-button {
+    box-sizing: border-box;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 20px auto;
+    background-color: $primary-color;
+    color: #fff;
+    width: 200px;
+    height: 47px;
+    text-align: center;
+    font-weight: bold;
+    border-radius: 50px;
+    transition-duration: .5s;
+    position: relative;
+    top: 0;
+
+    &:hover {
+      background-color: lighten($primary-color, 10%);
+      top: -5px;
+    }
+  }
+}
+
+.pokemon-card {
+  &-container {
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+  width: calc(100% / 4);
 }
 </style>

8cfa939971e1c1bf4906de2f6bcdf29e90588355
Tan Hernandez <tan.hernandez05@gmail.com>
2021-01-30 01:45:28 +0800

    Registered style resources.

--- nuxt.config.js
+++ nuxt.config.js
@@ -49,7 +49,8 @@ export default {
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios'
+    '@nuxtjs/axios',
+    '@nuxtjs/style-resources'
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
